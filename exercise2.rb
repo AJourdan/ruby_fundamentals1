f85c76f7c278fe32922733941c5a95e6f9c1ec66
Alon Jourdan <alon.jourdan@gmail.com>
2015-10-27 13:00:36 -0400

    exercise2.rb

---
+++ exercise2.rb
@@ -0,0 +1,23 @@
+
+#Question # 1
+
+puts 65*0.2
+
+#Question # 2
+
+number = 3
+
+puts "Hello, I really like the number #{number}"
+
+#Question # 3
+
+number = 45628 * 7839
+
+puts "45628 multiplied by 7839 equals #{number}"
+
+#Question # 4
+
+#The answer is TRUE, because it equals = False OR False OR True.
+#You only require one True when comparing "||"
+
+puts (true && false) || (false && true) || !(false && false)
