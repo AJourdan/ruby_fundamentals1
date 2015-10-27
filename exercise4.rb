a52296f784de41c6915d027cdf00f2b1de3d9c85
Alon Jourdan <alon.jourdan@gmail.com>
2015-10-27 14:20:44 -0400

    exercise4.rb

---
+++ exercise4.rb
@@ -0,0 +1,15 @@
+number = (1..100)
+
+number.each do |number|
+
+    if number%15 == 0
+	    puts "BitMaker"
+	elsif number%3 == 0
+		puts "Bit"
+	elsif number%5 == 0
+		puts "Maker"
+	else
+		puts number
+    end
+
+end
